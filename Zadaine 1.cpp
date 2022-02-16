2a3e9fdb6c3c1e392f0490c8d7a343dccb2fad1b
Mykyta Tsykhmenstruk <99841425+ZnikitoZ@users.noreply.github.com>
2022-02-16 22:57:46 +0100

    Create Zadaine 1.cpp

---
+++ Zadaine 1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+ using namespace std;
+ 
+int main(){
+int n, m, a;
+	cout<<"wpisz liczbe m"<< endl;
+		cin>>m;
+	cout<<"wpmisz liczbe n"<< endl;
+		cin>>n;
+if(m<=0 || n<=0)
+	cout<<"Nie mo¿esz wpisaæ liczbe ujemn¹"	<<endl;	
+	if(n>m)
+	cout<<"Nie mo¿esz wpisaæ liczbe n wiêkszej od m"<<endl;	
+a=1;
+	while(m>n*a){	
+		cout<<n*a<<endl;
+		a++; 
+	}		
+return 0;
+}
+
